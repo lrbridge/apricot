47219d0b8f8d2d9b05be0bd09c73571730dff8a7
lrbridge <lrbridge@users.noreply.github.com>
2015-10-03 13:20:03 -0700

    Initial commit with spot for part 1 and part 2, and tests for each

---
+++ test/part1/Part1Test.java
@@ -0,0 +1,14 @@
+package part1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Part1Test {
+
+	@Test
+	public void test() {
+		assertEquals(new Part1().solve(), 2);
+	}
+
+}
---
+++ test/part2/Part2Test.java
@@ -0,0 +1,16 @@
+package part2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import part1.Part1;
+
+public class Part2Test {
+
+	@Test
+	public void test() {
+		assertEquals(new Part1().solve(), 2);
+	}
+
+}
