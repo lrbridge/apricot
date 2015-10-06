6343cabd525d5351bdc8340cf434961c0a54d821
lrbridge <lrbridge@users.noreply.github.com>
2015-10-05 22:51:16 -0700

    Fixing tests for not checking every letter - no results change except order or way less nodes expanded

--- test/part1/Part1Test.java
+++ test/part1/Part1Test.java
@@ -10,44 +10,44 @@ public class Part1Test {
 	
 	@Test
 	public void trivial() {
-		String[] expectedWords = {"ANT", "APE", "BAD"};
-		testPart1("trivial.txt", "smallwordlist.txt", expectedWords, 156); //17
+		String[] expectedWords = {"APE", "ANT", "BAD"};
+		testPart1("trivial.txt", "smallwordlist.txt", expectedWords, 17);
 	}
 	
 	@Test
 	public void simple() {
 		String[] expectedWords = {"BAID"};
-		testPart1("simple.txt", "smallwordlist.txt", expectedWords, 130); //17
+		testPart1("simple.txt", "smallwordlist.txt", expectedWords, 17);
 	}
 	
 	@Test
 	public void puzzle1() {
 		String[] expectedWords = {"NNEMANDYE", "NNESAYDYE", "NWEMANDYE", "NWESAYDYE"};
-		testPart1("puzzle1.txt", "wordlist.txt", expectedWords, 15964); //9767
+		testPart1("puzzle1.txt", "wordlist.txt", expectedWords, 9767); 
 	}
 	
 	@Test
 	public void puzzle2() {
 		String[] expectedWords = {"HSIAIWNCS", "HSIAIWNPS", "HSIOIWNDS", "HSIOIWNYS"};
-		testPart1("puzzle2.txt", "wordlist.txt", expectedWords, 5590); //3384
+		testPart1("puzzle2.txt", "wordlist.txt", expectedWords, 3384); 
 	}
 	
 	@Test
 	public void puzzle3() {
 		String[] expectedWords = {"ASULPEA", "ASULPIE"};
-		testPart1("puzzle3.txt", "wordlist.txt", expectedWords, 4160); //3518
+		testPart1("puzzle3.txt", "wordlist.txt", expectedWords, 3518);
 	}
 	
 	@Test
 	public void puzzle4() {
 		String[] expectedWords = {"HEDITYRE", "HELITYRE", "HETITYRE"};
-		testPart1("puzzle4.txt", "wordlist.txt", expectedWords, 2938); //2144
+		testPart1("puzzle4.txt", "wordlist.txt", expectedWords, 2144); 
 	}
 	
 	@Test
 	public void puzzle5() {
 		String[] expectedWords = {"IHTTNOIEN", "IHTTYOIEN", "THTTNOIEN", "THTTYOIEN"};
-		testPart1("puzzle5.txt", "wordlist.txt", expectedWords, 134394); //71600
+		testPart1("puzzle5.txt", "wordlist.txt", expectedWords, 71600);
 	}
 
 	private void testPart1(String filename, String wordlist, String[] expectedWords, int expectedLinesInTrace) {
